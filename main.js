5c6ca27618670fd0fc4fe3a3ddd51ba710252697
Archie <archie.hamilton@eh9.co.uk>
2023-04-03 11:34:29 +0100

    forgot to make the repo before doing these. whoops

---
+++ main.js
@@ -0,0 +1,19 @@
+//global variables
+const one = document.getElementsByClassName('1');
+const two = document.getElementsByClassName('2');
+const three = document.getElementsByClassName('3');
+const four = document.getElementsByClassName('4');
+const five = document.getElementsByClassName('5');
+const six = document.getElementsByClassName('6');
+const seven = document.getElementsByClassName('7');
+const eight = document.getElementsByClassName('8');
+const nine = document.getElementsByClassName('9');
+const zero = document.getElementsByClassName('0');
+const allClear = document.getElementsByClassName('AC');
+const clear = document.getElementsByClassName('C');
+const multiply = document.getElementsByClassName('X');
+const divide = document.getElementsByClassName('÷');
+const add = document.getElementsByClassName('+');
+const subtract = document.getElementsByClassName('-');
+const equals = document.getElementsByClassName('=');
+
