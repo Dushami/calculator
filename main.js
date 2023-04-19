8d7e567964f478cdff5674889ef0c4404ad76902
Archie <archie.hamilton@eh9.co.uk>
2023-04-19 13:49:23 +0100

    fully functioning

--- main.js
+++ main.js
@@ -2,35 +2,35 @@
 let firstNum = ''
 let secondNum = ''
 let operation = null
-let screenReset = false
+let shouldResetScreen = false
 
 const numberButton = document.querySelectorAll('[data-number]');
-const operatorButton = document.querySelectorAll('[data-operator}');
-const allClear = document.getElementsByClassName('AC');
-const delete1 = document.getElementsByClassName('Delete');
-const equals = document.getElementsByClassName('=');
-const point = document.getElementsByClassName("point");
-const lastOperationScreen = document.getElementsByClassName('previousOperation');
-const currentOperationScreen = document.getElementsByClassName('currentOperaion');
+const operatorButton = document.querySelectorAll('[data-operator]');
+const allClear = document.getElementById('AC');
+const delete1 = document.getElementById('Delete');
+const equals = document.getElementById('equals');
+const point = document.getElementById("point");
+const lastOperationScreen = document.getElementById('previousOperation');
+const currentOperationScreen = document.getElementById('currentOperation');
 
 window.addEventListener('keydown', handleKeyboardInput)
-equals.addEventListener('click', evaluate);
-delete1.addEventListener('click', deleteANo);
+equals.addEventListener('click', evaluate)
+delete1.addEventListener('click', deleteNum);
 point.addEventListener('click', decimal);
 allClear.addEventListener('click', clearAll);
 
-numberButton.forEach((buton) => 
+numberButton.forEach((button) => 
     button.addEventListener('click', () => appendNumber(button.textContent))
 );
 
-operatorButton.forEach((buton) => 
-    button.addEventListener('click', () => setOperator(button.textContent))
+operatorButton.forEach((button) => 
+    button.addEventListener('click', () => setOperation(button.textContent))
 );
 
 function appendNumber(number){
     if (currentOperationScreen.textContent === '0' || shouldResetScreen)
         resetScreen();
-    lastOperationScreen.textContent += number;
+    currentOperationScreen.textContent += number;
 }
 
 function resetScreen(){
@@ -38,15 +38,15 @@ function resetScreen(){
     shouldResetScreen = false
 }
 
-function allClear(){
-    lastOperationScreen = 0;
-    currentOperationScreen = '';
+function clearAll(){
+    lastOperationScreen.textContent = '';
+    currentOperationScreen.textContent = '0';
     firstNum = '';
     secondNum = '';
     operation = null;
 }
 
-function apppendPoint(){
+function decimal(){
     if(shouldResetScreen) resetScreen();
     if(currentOperationScreen.textContent === ''){
         currentOperationScreen.textContent = '0';
@@ -56,7 +56,7 @@ function apppendPoint(){
     }
 }
 
-function delete1(){
+function deleteNum(){
     currentOperationScreen.textContent = currentOperationScreen.textContent
     .toString()
     .slice(0 , -1);
@@ -86,3 +86,57 @@ function roundedNum(number){
     return Math.round(number * 1000)/1000
 }
 
+function handleKeyboardInput(e){
+    if(e.key >= 0 && e.key <= 9) appendNumber(e.key);
+    if(e.key === '.') apppendPoint();
+    if(e.key === '=' || e.key === 'Enter') evaluate();
+    if(e.key === 'Backspace') deleteNum();
+    if(e.key === 'Escape') clearAll();
+    if(e.key ==='+' || e.key === '-' || e.key === '*' || e.key === 'x' || e.key === '/'){
+        setOperation(convertOperator(e.key));
+    }
+}
+
+function convertOperator(input){
+    if(input === '+') return '+';
+    if(input === '-') return '-';
+    if(input === '*') return '*';
+    if(input === 'x') return '*';
+    if(input === '/') return '÷';
+}
+
+function add(a , b){
+    return a + b;
+}
+
+function subtract(a , b){
+    return a - b;
+}
+
+function divide(a , b){
+    return a / b;
+}
+
+function multiply(a , b){
+    return a * b;
+}
+
+function operate(operator , a , b){
+    a = Number(a);
+    b = Number(b);
+    switch (operator){
+        case '÷':
+            if(b === 0) return null
+            else return divide(a, b);
+        case '*':
+            return multiply(a, b);
+        case 'x':
+            return multiply(a, b);
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a, b);
+        default:
+            return null;
+    }
+}
